b40ba2a0f2033fbff121ea25fbaa8f18496cd22f
Jacob Vallejo <you@jahkeup.com>
2018-05-13 14:28:36 -0700

    Attempt to remove unintended chord
    There are issues typing words like:
    
    controller
    tackle
    
    This is likely due to a timing issues between features or that are
    scanning the inputs and clearing state in a defined -- problematic --
    period.

--- hhkb/j/keymap.c
+++ hhkb/j/keymap.c
@@ -3,8 +3,22 @@
 #define BASE 0
 #define HHKB 1
 
+/* These issues are related to something wrt timing with the
+   controller used by the hhkb alt controller:
+
+   https://github.com/qmk/qmk_firmware/issues/1256
+
+   Defining IGNORE_MOD_TAP_INTERRUPT makes it possible to do rolling
+   combos (zx) with keys that convert to other keys on hold (z becomes
+   ctrl when you hold it, and when this option isn't enabled, z
+   rapidly followed by x actually sends Ctrl-x. That's bad.)
+*/
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef DEBOUNCE
+#define DEBOUNCE 15
+
 #undef TAPPING_TERM
-#define TAPPING_TERM 250
+#define TAPPING_TERM 150
 #define RETRO_TAPPING yes
 #define PERMISSIVE_HOLD yes
 
