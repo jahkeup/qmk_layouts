b297fc2c0c3d7806d8de7282463181bf39b8e822
Jacob Vallejo <you@jahkeup.com>
2018-09-17 08:00:33 -0700

    Disable RETRO_TAPPING, no longer needed
    With a tiling window manager, I do not need to press GUI (which was a
    dual function key) on its own.

--- ergodox/j/config.h
+++ ergodox/j/config.h
@@ -2,12 +2,6 @@
 // is not registered as a tap.
 #undef  TAPPING_TERM
 #define TAPPING_TERM 250
-// When you hold a dual function key, and haven't pressed anything
-// when you release the key, normally nothing happens. However, if you
-// enable this, if you release the key without pressing another key,
-// it will send the original key, even if it is outside of the tapping
-// term.
-#define RETRO_TAPPING yes
 // This makes it easier for fast typists to use dual-function
 // keys. Without this, if you let go of a held key inside the tapping
 // term, it won't register.
