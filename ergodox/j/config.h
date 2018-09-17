7a96f81891af590aaa562aeea1471273b30aaf57
Jacob Vallejo <you@jahkeup.com>
2018-09-16 20:33:13 -0700

    Remove autoshift and cleanup to fix layout macro
    The changes in 698ce733b73a2a8d6d41942b514af4c9d45190df caused this
    code to stop working as the macro changed from KEYMAP to
    LAYOUT_ergodox.
    
    Auto shift was causing some issues and was detrimental to my ability
    to type.

--- ergodox/j/config.h
+++ ergodox/j/config.h
@@ -1,6 +1,20 @@
+// TAPPING_TERM defines the duration of a "tap" where when exceeded,
+// is not registered as a tap.
 #undef  TAPPING_TERM
 #define TAPPING_TERM 250
+// When you hold a dual function key, and haven't pressed anything
+// when you release the key, normally nothing happens. However, if you
+// enable this, if you release the key without pressing another key,
+// it will send the original key, even if it is outside of the tapping
+// term.
 #define RETRO_TAPPING yes
+// This makes it easier for fast typists to use dual-function
+// keys. Without this, if you let go of a held key inside the tapping
+// term, it won't register.
 #define PERMISSIVE_HOLD yes
-// I want to use this for typing things like ~
-#undef NO_AUTO_SHIFT_SPECIAL
+// When a dual role key used for a modifier is quickly followed by
+// another keys, it is interpreted as held even before the tapping
+// term elapsed. This is a problem if a key is used for example inside
+// a rolling combo because the second key will be pressed before the
+// first key is released.
+#define IGNORE_MOD_TAP_INTERRUPT
